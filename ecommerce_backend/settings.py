9c99bab879e7d99423effefbf998e239e5424755
Muhammed ebrahim <ebrahimmuhammed479@gmail.com>
2024-09-22 13:37:28 +0300

    authentication source

--- ecommerce_backend/settings.py
+++ ecommerce_backend/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = "django-insecure-0k#vd2t_%yx@w*#fu*0kiy71!ak(3-c=yxet-x4kint+82_bbi
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -40,7 +38,16 @@ INSTALLED_APPS = [
     "rest_framework",
     "user",
     "product",
-    "order"
+    "order",
+    "django.contrib.sites",
+    "allauth",
+    "allauth.account",
+    "allauth.socialaccount",
+    "allauth.socialaccount.providers.google",  # Add other providers like Facebook, Twitter
+    "allauth.socialaccount.providers.facebook",  # Add other providers like Facebook, Twitter
+    "allauth.socialaccount.providers.twitter",  # Add other providers like Facebook, Twitter
+    "allauth.socialaccount.providers.apple",  # Add other providers like Facebook, Twitter
+    "rest_framework_simplejwt.token_blacklist",
 ]
 
 MIDDLEWARE = [
@@ -73,7 +80,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "ecommerce_backend.wsgi.application"
 
-
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
@@ -83,7 +89,6 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
@@ -103,7 +108,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
@@ -114,7 +118,6 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
